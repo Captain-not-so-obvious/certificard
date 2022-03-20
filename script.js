5ea50d92fd03b486b00d03722b971607eee6ef07
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2022-03-20 12:34:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+
+//Terminar todo o design que está no figma (disponível abaixo)
+//Adicionar suas redes sociais de forma que quem clique consiga abrí-las
